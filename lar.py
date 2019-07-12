ce8d7b40aefe21cc814c51c9f3d03d98e0d6d3fd
Varatharajan72 <51222666+Varatharajan72@users.noreply.github.com>
2019-07-12 02:39:34 -0700

    Update and rename program.py to lar.py

---
+++ lar.py
@@ -0,0 +1,8 @@
+x,y,z=input().split()
+if(x>=y) and (x>=z):
+    lar=x
+elif(y>=x) and (y>=z):
+    lar=y
+else:
+    lar=z
+print(lar)
--- program.py
+++
@@ -1,8 +0,0 @@
-a,b,c=input().split()
-if(a>=b and a>=c):
-    largest=a
-elif(b>=a and b>=c):
-    largest=b
-else:
-    largest=c
-print(largest)
